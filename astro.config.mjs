2968206b4afa927bd0d14547983c3cd1eaf3edd8
imossaidqadri <imossaidquadri@gmail.com>
2025-06-18 20:55:48 +0500

    created server side redirect / => /projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -11,10 +13,14 @@ export default defineConfig({
       applyBaseStyles: false // Keep your custom global styles
     })
   ],
+
   output: 'server',
+
   vite: {
     define: {
       'import.meta.env.DEV': JSON.stringify(process.env.NODE_ENV === 'development')
     }
-  }
+  },
+
+  adapter: vercel()
 });
